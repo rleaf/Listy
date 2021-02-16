8162b104d41605088d8f4c73ae8294f4f831b18b
Rleaf <calcifer44@gmail.com>
2021-02-15 21:43:53 -0500

    Added trycatch for better userexperience & improved -remove

--- ye.js
+++ ye.js
@@ -23,7 +23,11 @@ client.on('message', async msg => {
    const args = msg.content.split(' ').splice(1).join(' ');
 
    if(msg.content === `${prefix}help`) {
-      msg.channel.send(`${prefix}list, ${prefix}add, ${prefix}remove, ${prefix}finished`);
+      try {
+         msg.channel.send(`${prefix}list, ${prefix}add, ${prefix}remove, ${prefix}finished`);
+      } catch (err) {
+         msg.channel.send(`I've run into an error: ` +  err);
+      }
    }
 
 
@@ -38,60 +42,87 @@ client.on('message', async msg => {
 
 
    if (msg.content === `${prefix}list`) {
-      const listembed = new Discord.MessageEmbed()
-         .setTitle("Moovie List!")
-         .setColor(0x51ED26)
-         .setURL('https://www.youtube.com/watch?v=j0lN0w5HVT8')
-         .setFooter('Listy', 'https://cdn.discordapp.com/avatars/709934332529213540/4de87717e63539f57f302c8eeef8e458.png')
-         .setDescription(fs.readFileSync('watch.txt', 'utf-8'));
-
-      msg.channel.send(listembed);
+      try {
+         const listembed = new Discord.MessageEmbed()
+            .setTitle("Moovie List!")
+            .setColor(0x51ED26)
+            .setURL('https://www.youtube.com/watch?v=j0lN0w5HVT8')
+            .setFooter('Listy', 'https://cdn.discordapp.com/avatars/709934332529213540/4de87717e63539f57f302c8eeef8e458.png')
+            .setDescription(fs.readFileSync('watch.txt', 'utf-8'));
+   
+         msg.channel.send(listembed);
+      } catch (err) {
+         msg.channel.send(`I've run into an error: ` +  err);
+      }
    }
 
 
    if (msg.content.startsWith(`${prefix}add`)) {
-      if (msg.member.roles.cache.find(r => r.name === "New Bois")) {
-         msg.channel.send(`no >:(`)
-      } else {
-         fs.appendFile('watch.txt', args + "\n", 'utf-8', (err) => {
-            if (err) throw err;
-            console.log('appended successfully.');
-         });
-         msg.channel.send(reply[Math.floor(Math.random() * (reply.length))]);
+      try {
+         // Forbid anybody with the role 'New Bois' from adding movies to the list
+         if (msg.member.roles.cache.find(r => r.name === "New Bois")) {
+            msg.channel.send(`no >:(`)
+         } else {
+            fs.appendFile('watch.txt', args + "\n", 'utf-8', (err) => {
+               if (err) throw err;
+               console.log('appended successfully.');
+            });
+            msg.channel.send(reply[Math.floor(Math.random() * (reply.length))]);
+         }
+      } catch (err) {
+         msg.channel.send(`I've run into an error: ` +  err);
       }
    }
 
 
    if (msg.content.startsWith(`${prefix}remove`)) {
-      fs.readFile('watch.txt', 'utf-8', function (err, data) {
-         if (err) throw error;
-
-         let newMovie = data.replace(args + '\n', '');
-         let removeWhite = newMovie.replace(/^\s*[\r\n]/gm, '');
-         // let removeWhite = newMovie.replace(/^.*args*$/mg, '');
-
-         fs.writeFile('watch.txt', removeWhite, (err) => {
-            if (err) throw err;
-            console.log("updated file");
+      try {
+         fs.readFile('watch.txt', 'utf-8', function (err, data) {
+            if (err) throw error;
+   
+            // Check to see if the argument being passed is in the text file
+            if (data.includes(args)) {
+               console.log('That file is in watch.txt');
+   
+               // Replace the argument with empty string
+               let newMovie = data.replace(args, '');
+               // Removes any blank lines. https://stackoverflow.com/questions/16369642/javascript-how-to-use-a-regular-expression-to-remove-blank-lines-from-a-string
+               let removeWhite = newMovie.replace(/^\s*[\r\n]/gm, '');
+   
+               fs.writeFile('watch.txt', removeWhite, (err) => {
+                  if (err) throw err;
+                  console.log("updated file");
+               });
+   
+               fs.appendFileSync('finished.txt', args + "\n");
+         
+               msg.channel.send(`Moved **${args}** to finished! ` + reply[Math.floor(Math.random() * (reply.length))]);
+            } else {
+               msg.channel.send(`That movie isn't in the list baka dumbass - make sure you spelt it right and/or check capitalization.`)
+            }
+   
          });
-      });
+      } catch (err) {
+         msg.channel.send(`I've run into an error: ` +  err);
+      }
 
-      fs.appendFileSync('finished.txt', args + "\n");
-
-      msg.channel.send(reply[Math.floor(Math.random() * (reply.length))]);
    }
 
 
    if (msg.content.startsWith(`${prefix}finished`)) {
-      const finembed = new Discord.MessageEmbed()
-         .setTitle("Finished Mowovies")
-         .setColor(0xD25E5E)
-         .setURL('https://youtu.be/qTsaS1Tm-Ic?t=10')
-         .setFooter('Listy', 'https://cdn.discordapp.com/avatars/709934332529213540/4de87717e63539f57f302c8eeef8e458.png')
-         .setDescription(fs.readFileSync('finished.txt', 'utf-8'));
-
-      msg.channel.send(finembed);
-      // msg.author.send(finembed);
+      try {
+         const finembed = new Discord.MessageEmbed()
+            .setTitle("Finished Mowovies")
+            .setColor(0xD25E5E)
+            .setURL('https://youtu.be/qTsaS1Tm-Ic?t=10')
+            .setFooter('Listy', 'https://cdn.discordapp.com/avatars/709934332529213540/4de87717e63539f57f302c8eeef8e458.png')
+            .setDescription(fs.readFileSync('finished.txt', 'utf-8'));
+   
+         msg.channel.send(finembed);
+         // msg.author.send(finembed);
+      } catch (err) {
+         msg.channel.send(`I've run into an error: ` +  err);
+      }
    }
 });
 
